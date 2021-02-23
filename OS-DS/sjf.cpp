4488f95a38d62d6b8a12aabb1c505888ca72a3b3
Abhilasha Bansal <64535297+AbhilashaBansal@users.noreply.github.com>
2021-02-23 11:28:39 +0530

    Update sjf.cpp

--- OS-DS/sjf.cpp
+++ OS-DS/sjf.cpp
@@ -81,11 +81,11 @@ int main() {
         int procid = cur_proc.first;
         int arrt = cur_proc.second.first;
         int bt = cur_proc.second.second;
-        waiting_times[procid] = current_time-arrt;
-        turnaround_times[procid] = waiting_times[procid]+bt;
-        current_time += bt;
+        waiting_times[procid-1] = current_time-arrt;
+        turnaround_times[procid-1] = waiting_times[procid-1]+bt;
         cout<<left<<setw(10)<<current_time<<setw(25)<<procid<<setw(20)<<arrt<<setw(20)<<bt;
         cout<<endl;
+        current_time += bt;
     }
     cout<<"\nEnd Time: "<<current_time<<endl<<endl;
 
@@ -95,11 +95,18 @@ int main() {
     // --> list of processes in ready queue, with arrival & burst times
     // At the end, show waiting and turnaround time for each process
 
+    float avg_waiting_time=0, avg_ta_time=0;
     cout<<left<<setw(20)<<"Process ID"<<setw(20)<<"Waiting Time"<<setw(10)<<"Turnaround Time\n";
     for(int i=0; i<n; i++){
         cout<<left<<setw(20)<<i+1<<setw(20)<<waiting_times[i]<<setw(10)<<turnaround_times[i]<<endl;
+        avg_waiting_time+=waiting_times[i];
+        avg_ta_time+=turnaround_times[i];
     }
     cout<<endl;
+    avg_waiting_time /= n;
+    avg_ta_time /= n;
+    cout<<endl<<"Average Waiting Time: "<<avg_waiting_time<<endl;
+    cout<<"Average Turnaround Time: "<<avg_ta_time<<endl<<endl;
 
     return 0;
 }
